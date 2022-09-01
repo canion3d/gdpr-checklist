80827db8df04ad887c8d4e9aa5fb8c3e0561f446
Canion3D <80270614+canion3d@users.noreply.github.com>
2022-09-01 12:40:07 +0200

    Add files via upload

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,45 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: 'The GDPR Checklist',
-    siteUrl: 'https://gdprchecklist.io'
+    title: `Gatsby Default Starter`,
   },
-  plugins: [
-    'gatsby-plugin-react-helmet',
-    `gatsby-plugin-emotion`,
-    `gatsby-plugin-sitemap`,
-    `gatsby-plugin-offline`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        path: `${__dirname}/src/images`,
-        name: 'images',
-      },
-    },
-    {
-      resolve: 'gatsby-plugin-robots-txt',
-      options: {
-        host: 'https://gdprchecklist.io',
-        sitemap: 'https://gdprchecklist.io/sitemap.xml',
-        policy: [{ userAgent: '*', allow: '/' }]
-      }
-    },
-    {
-      resolve: 'gatsby-plugin-favicon',
-      options: {
-        logo: './src/images/favicon.png',
-        injectHTML: true,
-        icons: {
-          android: true,
-          appleIcon: true,
-          appleStartup: true,
-          coast: false,
-          favicons: true,
-          firefox: true,
-          twitter: true,
-          yandex: false,
-          windows: false
-        }
-      }
-    },
-  ],
-};
+  plugins: [`gatsby-plugin-react-helmet`],
+}
